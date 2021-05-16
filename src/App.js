03ef86ac8f845c5591087dc9560b9a9fd7241df7
lavina <lavinakhushlani@gmail.com>
2021-05-16 16:41:17 +0530

    Public dir

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">  
+      {/* div injected in html file  */}
+
+<div className="content">
+  <h1> App component</h1>
+  </div>
+    </div>
+  );
+}
+
+export default App;
