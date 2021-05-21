cf6fcf3c57d1341bd470a8c1e5a83c1acb8e525c
lavina <lavinakhushlani@gmail.com>
2021-05-21 12:03:34 +0530

    List of blogs

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,24 @@
+import { useState } from 'react'
+
 const Home = () => {
+    
+    const [blogs, setBlogs] = useState([
+        {title : 'My New Blog', body : 'lorem .. ipsum', author: 'lavina', id: 1},      
+        {title : 'Welcome party', body : 'lorem .. ipsum', author: 'dan brown', id: 2},
+        {title : 'Web dev tips', body : 'lorem .. ipsum', author: 'stacy', id: 3}    
+        ]);
     return (  
         <div className="home">
-            <h2> Home Page </h2>
+{/* loop through the array and generates a template  using JS using map method*/}
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.idx}>
+                    <h2> { blog.title }</h2>
+                    <p>Written by { blog.author } </p>
+
+                </div>
+      
+            ))}
+         
         </div>
 
     );
