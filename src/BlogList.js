89a7e5f9fdb57f131d63a9c7310697e53b739639
lavina <lavinakhushlani@gmail.com>
2021-05-23 18:09:43 +0530

    Props as functions

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,15 +1,15 @@
-const BlogList = ( { blogs }) => {   // decomposigng props in  parenthesis
+const BlogList = ( { blogs, title,  handleDelete }) => {   // decomposigng props in  parenthesis
     //  2 ways to know 
     // const BlogList = (props) => {
         // const blogs = props.blogs;
 console.log(blogs);
     return (  
         <div className='blog-list'>
-
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2> { blog.title }</h2>
                     <p>Written by { blog.author } </p>
+                    <button onClick={ () => handleDelete(blog.id)}>Delete Blog </button>
 
                 </div>
       
--- src/Home.js
+++ src/Home.js
@@ -6,12 +6,22 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         {title : 'My New Blog', body : 'lorem .. ipsum', author: 'lavina', id: 1},      
         {title : 'Welcome party', body : 'lorem .. ipsum', author: 'dan brown', id: 2},
-        {title : 'Web dev tips', body : 'lorem .. ipsum', author: 'stacy', id: 3}    
+        {title : 'Web dev tips', body : 'lorem .. ipsum', author: 'lavina', id: 3}    
         ]);
+    
+    const handleDelete = (id) => {
+
+        const newBlogs = blogs.filter( (blog) => blog.id != id );
+        setBlogs(newBlogs);
+
+    }
+    
+
     return (  
         <div className="home">
 {/* loop through the array and generates a template  using JS using map method*/}
-            <BlogList blogs={blogs}/> 
+            <BlogList blogs={blogs} title="All Blogs" handleDelete = {handleDelete}/> 
+            {/* <BlogList blogs={blogs.filter((blog) => blog.author == 'lavina')} title="Filtered Blogs"/>   */}
          {/* props used here props passed here from parent to child component */}
         </div>
 
