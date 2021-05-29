9313561185d4d814d297853d63f9362b2bbf2bef
lavina <lavinakhushlani@gmail.com>
2021-05-29 11:09:14 +0530

    UseEffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -8,21 +8,27 @@ const Home = () => {
         {title : 'Welcome party', body : 'lorem .. ipsum', author: 'dan brown', id: 2},
         {title : 'Web dev tips', body : 'lorem .. ipsum', author: 'lavina', id: 3}    
         ]);
+
+    const [name, setName] = useState('mario');
     
     const handleDelete = (id) => {
-
         const newBlogs = blogs.filter( (blog) => blog.id != id );
         setBlogs(newBlogs);
-
     }
+
+    useEffect(() => {
+        console.log("use effect");
+    }, [name]);
     
 
     return (  
         <div className="home">
-{/* loop through the array and generates a template  using JS using map method*/}
+            {/* loop through the array and generates a template  using JS using map method*/}
             <BlogList blogs={blogs} title="All Blogs" handleDelete = {handleDelete}/> 
             {/* <BlogList blogs={blogs.filter((blog) => blog.author == 'lavina')} title="Filtered Blogs"/>   */}
-         {/* props used here props passed here from parent to child component */}
+            {/* props used here props passed here from parent to child component */}
+            <button onClick={ () => setName('luigi')}>Change Name</button>
+            <p> { name }</p>
         </div>
 
     );
